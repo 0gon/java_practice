fde12c280e9e34691dfc205e24875e90b4a38201
Renovo <safoma@naver.com>
2019-09-25 21:07:02 +0900

    foreach와 람다를 이용하여 list 출력

---
+++ src/chapter/ForeachLambda.java
@@ -0,0 +1,18 @@
+package chapter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ForeachLambda {
+	public static void main(String[] args) {
+		List<Integer> mmlists = Arrays.asList(1,6,16,22,23,33);
+		//#1
+		mmlists.forEach(m->{System.out.print(m+"\t");});
+		System.out.println();
+		//#2 같은결과 다른 방식
+		Consumer<Integer> consume = (Integer m)->{System.out.print(m+"\t");};
+		mmlists.forEach(consume);
+		
+	}
+}
