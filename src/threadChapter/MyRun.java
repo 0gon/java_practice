4b1a9d15d0ea55bee8fe34bc5f080eee30e73af5
Renovo <safoma@naver.com>
2019-09-29 22:06:47 +0900

    tread 4가지방식

---
+++ src/threadChapter/MyRun.java
@@ -0,0 +1,17 @@
+package threadChapter;
+
+public class MyRun implements Runnable{
+
+	@Override
+	public void run() {
+		show();
+	}
+	
+	public void show() {
+		for(int i = 0 ; i < 500; i++) {
+			System.out.print("S");
+		}
+	}
+	
+
+}
