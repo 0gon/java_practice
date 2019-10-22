532452691202cdfe4261b7c2fbf1dbedfc3b0f63
Renovo <safoma@naver.com>
2019-10-22 21:40:39 +0900

    wait notifiyAll method 2

---
+++ src/threadChapter/unit4/CakeMaker.java
@@ -0,0 +1,19 @@
+package threadChapter.unit4;
+
+public class CakeMaker extends Thread{
+	private CakePlate cake;
+	public CakeMaker(CakePlate cake) {
+		setCakePlate(cake);
+	}
+	private void setCakePlate(CakePlate cake) {
+		this.cake=cake;
+	}
+	public CakePlate getCakePlate() {
+		return cake;
+	}
+	public void run() {
+		for(int i=0;i<50;i++) {
+			cake.makeBread();
+		}
+	}
+}
