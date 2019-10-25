e8666567e6fef715ea30ac018dc77415cc0eb467
Renovo <safoma@naver.com>
2019-10-25 19:45:44 +0900

    wait notifiyAll

---
+++ src/threadChapter/unit4/CakeEater.java
@@ -0,0 +1,21 @@
+package threadChapter.unit4;
+
+public class CakeEater extends Thread {
+	private CakePlate cake;
+	public CakeEater(CakePlate cake) {
+		setCakePlate(cake);
+	}
+	public void setCakePlate(CakePlate cake) {
+		this.cake=cake;
+	}
+	public CakePlate getCakePlate() {
+		return cake;
+	}
+	
+	public void run() {
+		for(int i=0;i<50;i++) {
+			cake.eatBread();
+		}
+	}
+
+}
