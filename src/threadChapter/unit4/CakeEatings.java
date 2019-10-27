52c925bd573f9ff9e808e6b22f3ed1e490267aa0
Renovo <safoma@naver.com>
2019-10-27 20:43:19 +0900

    쓰레드 wait final

---
+++ src/threadChapter/unit4/CakeEatings.java
@@ -0,0 +1,12 @@
+package threadChapter.unit4;
+
+public class CakeEatings {
+	public static void main(String[] args) {
+		CakePlate cake = new CakePlate();
+		CakeEater eater = new CakeEater(cake);
+		CakeMaker baker = new CakeMaker(cake);
+		
+		baker.start();
+		eater.start();
+	}
+}
